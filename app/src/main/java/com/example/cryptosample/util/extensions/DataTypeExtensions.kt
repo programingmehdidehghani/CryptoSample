46cf5762f880333b2a2a9cd21b71b705067d24a4
mehdi <dehghani_m@rocketmail.com>
2023-04-07 19:01:00 +0430

    make extension

---
+++ app/src/main/java/com/example/cryptosample/util/extensions/DataTypeExtensions.kt
@@ -0,0 +1,32 @@
+package com.example.cryptosample.util.extensions
+
+import android.annotation.SuppressLint
+import android.icu.text.DateFormat
+import android.icu.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun String?.emptyIfNull(): String {
+    return this ?: ""
+}
+
+fun String?.trimParanthesis(): String {
+    return this?.replace(Regex("[()]"), "") ?: ""
+}
+
+@SuppressLint("SimpleDateFormat")
+fun String?.formattedDate(dateFormat: DateFormat = DateFormat.yyyy_MM_dd): Date? =
+    this?.let {
+        try {
+            SimpleDateFormat(dateFormat.format).parse(this)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+fun Double?.dollarString(): String {
+    return this?.let {
+        val numberFormat = DecimalFormat("#,##0.00")
+        "US$ ${numberFormat.format(this)}"
+    } ?: ""
+}
