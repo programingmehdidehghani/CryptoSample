4fa1280d2e246e630d1c66a03946cfb131cf5447
mehdi <dehghani_m@rocketmail.com>
2023-04-07 18:47:57 +0430

    make application class

---
+++ app/src/main/java/com/example/cryptosample/MyApp.kt
@@ -0,0 +1,20 @@
+package com.example.cryptosample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MyApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
