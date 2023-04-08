d250c6eb96452b57718a27c05a4834a56bebbe87
mehdi <dehghani_m@rocketmail.com>
2023-04-08 15:06:51 +0430

    continue navigation

--- app/src/main/java/com/example/cryptosample/ui/home/coinsList/CoinListFragment.kt
+++ app/src/main/java/com/example/cryptosample/ui/home/coinsList/CoinListFragment.kt
@@ -7,9 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cryptosample.R
 import com.example.cryptosample.core.common.MainNavigationFragment
+import com.example.cryptosample.databinding.FragmentListBinding
 import com.example.cryptosample.util.Constants
+import com.example.cryptosample.util.extensions.doOnChange
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_list.*
 
 @AndroidEntryPoint
 class CoinListFragment : MainNavigationFragment(), OnItemClickCallback {
