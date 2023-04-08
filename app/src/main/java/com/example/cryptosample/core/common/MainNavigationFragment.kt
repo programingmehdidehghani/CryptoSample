ce0efc8f7f5a44ae131b2e29be34a00d8f0f79df
mehdi <dehghani_m@rocketmail.com>
2023-04-08 13:51:15 +0430

    start make xml

---
+++ app/src/main/java/com/example/cryptosample/core/common/MainNavigationFragment.kt
@@ -0,0 +1,41 @@
+package com.example.cryptosample.core.common
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
+import com.example.cryptosample.R
+
+interface NavigationHost {
+    fun registerToolbarWithNavigation(toolbar: Toolbar)
+}
+
+/**
+ * [MainNavigationFragment] registers the toolbar from the fragment with the activity
+ */
+abstract class MainNavigationFragment : BaseFragment() {
+
+    private var navigationHost: NavigationHost? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is NavigationHost) {
+            navigationHost = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        navigationHost = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val host = navigationHost ?: return
+
+        val mainToolbar: Toolbar = view.findViewById(R.id.toolbar) ?: return
+        mainToolbar.apply {
+            host.registerToolbarWithNavigation(this)
+        }
+    }
+}
