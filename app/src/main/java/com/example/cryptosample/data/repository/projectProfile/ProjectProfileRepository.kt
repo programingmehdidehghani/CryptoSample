11c83042587e2f304cde5977988e58d5d4863bc6
mehdi <dehghani_m@rocketmail.com>
2023-04-08 13:42:12 +0430

    navigation

--- app/src/main/java/com/example/cryptosample/data/repository/projectProfile/ProjectProfileRepository.kt
+++ app/src/main/java/com/example/cryptosample/data/repository/projectProfile/ProjectProfileRepository.kt
@@ -1,4 +1,14 @@
 package com.example.cryptosample.data.repository.projectProfile
 
-class ProjectProfileRepository {
+import javax.inject.Inject
+
+class ProjectProfileRepository @Inject constructor(
+    private val localDataSource: ProjectProfileDataSource,
+    private val remoteDataSource: ProjectProfileRemoteDataSource
+) {
+
+    fun projectBySymbol(symbol: String) = localDataSource.projectBySymbol(symbol)
+
+    suspend fun historicalPrice(symbol: String, targetCur: String = "usd") =
+        remoteDataSource.historicalPrice(symbol, targetCur)
 }
