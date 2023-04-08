b1a2b35ce7953cf8cc48c3b6e1955dab8f6bdd67
mehdi <dehghani_m@rocketmail.com>
2023-04-08 12:12:59 +0430

    continue db

--- app/src/main/java/com/example/cryptosample/data/local/database/CoinsDatabase.kt
+++ app/src/main/java/com/example/cryptosample/data/local/database/CoinsDatabase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.cryptosample.data.local.database.DB.DATABASE_NAME
+import com.example.cryptosample.data.local.database.DB.DATABASE_VERSION
 
 @Database(entities = [CoinsListEntity::class], version = DATABASE_VERSION, exportSchema = false)
 abstract class CoinsDatabase  : RoomDatabase() {
---
+++ app/src/main/java/com/example/cryptosample/data/local/database/DB.kt
@@ -0,0 +1,6 @@
+package com.example.cryptosample.data.local.database
+
+object DB {
+    const val DATABASE_NAME = "Coins.DB"
+    const val DATABASE_VERSION = 1
+}
