02e28d9e5085fa8cedbf21e868e2a3f57f488885
mehdi <dehghani_m@rocketmail.com>
2023-04-08 13:47:17 +0430

    base fragment

---
+++ app/src/main/java/com/example/cryptosample/core/common/BaseFragment.kt
@@ -0,0 +1,20 @@
+package com.example.cryptosample.core.common
+
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+interface InitViews {
+    fun initializeViews()
+    fun observeViewModel()
+}
+
+abstract class BaseFragment : Fragment(), InitViews {
+
+    fun showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(
+            context,
+            message,
+            duration
+        ).show()
+    }
+}
